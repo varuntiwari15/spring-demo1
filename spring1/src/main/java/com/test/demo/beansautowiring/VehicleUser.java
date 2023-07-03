6687082a65d928492a34a03010f30a6be9c7413e
varun tiwari <varuntiwari15@gmail.com>
2023-07-03 01:56:53 -0500

    modified comments

--- spring1/src/main/java/com/test/demo/beansautowiring/VehicleUser.java
+++ spring1/src/main/java/com/test/demo/beansautowiring/VehicleUser.java
@@ -7,9 +7,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class VehicleUser {
-    @Autowired // ambiguity as three types of vehicles- Car, Bike, Cycle
+    @Autowired // ambiguity as three types of vehicles- Car, Bike, Cycle, also can user JSr-@Inject
     @Qualifier("cycle-bean")  // need to give any one type of impl of vehicle and this
-    // combination can be used with CI, SI also
+    // combination can be used with CI, SI also, also this combination
+    // instead of this combination (@Autowired and @Qualifier("test")) we can also use
+    // JSR @Resource("test") annotation
+    // or @Inject with @Qualifier
+    // or @Inject and @Named === @Qualifier
+    // also we can use @Primary to denote priority bean selection..like for Bike @Primary
+    // then remove Qualifier with name and use only @Primary at Bike class
     private Vehicle vehicle;
 
     public void showVehicleName(){
--- spring1/src/main/java/com/test/demo/service/impl/Bike.java
+++ spring1/src/main/java/com/test/demo/service/impl/Bike.java
@@ -1,9 +1,11 @@
 package com.test.demo.service.impl;
 
 import com.test.demo.service.Vehicle;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component("bike-bean")
+//@Primary  use it if dont want to use Qualifier
 public class Bike implements Vehicle {
     @Override
     public void name() {
